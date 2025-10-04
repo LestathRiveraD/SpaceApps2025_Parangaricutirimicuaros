91391452ec10856c70d524114bd65758008294e8
LestathRiveraD <cesarrivera0404@gmail.com>
2025-10-04 11:53:34 -0700

    ahora si

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p>holamundo</p>  
+    </>
+  )
+}
+
+export default App
